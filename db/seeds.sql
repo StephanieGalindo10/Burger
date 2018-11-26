4e53a870afd419af69efab110b47069b57f04be3
test <test@gmail.com>
2018-11-25 17:26:27 -0800

    added sql schema need a review

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Veggie Paradise Burger", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Hangover Sunrise Burger", TRUE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Little Tokyo 100% Wagyu Beef Burger", FALSE);
