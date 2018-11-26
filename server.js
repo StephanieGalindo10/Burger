696e5e80a59c78bd9cc10731c94500a6c918037e
test <test@gmail.com>
2018-11-25 18:02:15 -0800

    created connection.js

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 var express = require("express");
 
-
-
 var bodyParser = require("body-parser");
+
 ///var port will be 3000(Local Host)///
 var PORT = process.env.PORT || 3000;
 
@@ -20,13 +19,14 @@ var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({
   defaultLayout: "main"
 }));
+
 app.set("view engine", "handlebars");
 
 
 var routes = require("./controllers/burgers_controller.js");
 app.use(routes);
 
-// start the server listening
+
 app.listen(PORT, function() {
   console.log("App now listening at localhost:" + PORT);
 });
