c0e5e66d27d8713c26f892c0ea05c65d1727efa3
test <test@gmail.com>
2018-11-27 17:59:10 -0800

    added notes and clean code

--- models/burger.js
+++ models/burger.js
@@ -1,17 +1,23 @@
 
 var orm = require("../config/orm.js");
 
+//selectall
+
 var burger = {
   selectAll: function(cb) {
     orm.selectAll("burgers", function(res) {
       cb(res);
     });
   },
+
+  //insertOne
   insertOne: function(cols, vals, cb) {
     orm.insertOne("burgers", cols, vals, function(res) {
       cb(res);
     });
   },
+
+  ///updateOne
   updateOne: function(objColVals, condition, cb) {
     orm.updateOne("burgers", objColVals, condition, function(res) {
       cb(res);
