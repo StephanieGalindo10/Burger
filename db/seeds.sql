df0165f848e1a674b1767beb42764ea969d9058e
test <test@gmail.com>
2018-11-29 18:03:14 -0800

    added new burger db

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,4 @@
 
 INSERT INTO burgers (burger_name, devoured) VALUES ("Veggie Paradise Burger", FALSE);
-INSERT INTO burgers (burger_name, devoured) VALUES ("Hangover Sunrise Burger", TRUE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Steakhouse (Brooklyn Style) Burger", TRUE);
 INSERT INTO burgers (burger_name, devoured) VALUES ("Little Tokyo 100% Wagyu Beef Burger", FALSE);
