7fcd7acf4a05c57e26d12f38da127da0fc9f232b
test <test@gmail.com>
2018-12-01 20:23:46 -0800

    change code

--- server.js
+++ server.js
@@ -33,6 +33,6 @@ app.use(routes);
 
 
 
-app.listen(PORT, function() {
-  console.log("App now listening at localhost:" + PORT);
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
